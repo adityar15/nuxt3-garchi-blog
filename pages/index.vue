0c680edf657a6ee715b61f3105323cec4fc3118c
Aditya Kadam <adityakadam@Adityas-MacBook-Air.local>
2023-09-15 16:46:29 +0100

    script tag added

--- pages/index.vue
+++ pages/index.vue
@@ -2,3 +2,15 @@
    <Page slug="/" />
 </template>
 
+<script setup>
+// can move this to app.vue if needed
+// learn more at https://garchi.co.uk/documentation/1.0/headlessweb/nuxt
+useHead({
+    script: [
+        {
+            src: "https://garchi.co.uk/script/index.es.js",
+            defer: true,
+        }
+    ]
+})
+</script>
